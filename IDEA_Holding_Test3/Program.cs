fa25d84fb10e398d692052c6475dda0c03745c14
fejzo9 <fejzo999@gmail.com>
2025-02-17 00:17:26 +0100

    Changed from Program.cs making a problem. Has errors. Need to fix it.

--- IDEA_Holding_Test3/Program.cs
+++ IDEA_Holding_Test3/Program.cs
@@ -1,14 +1,38 @@
-using IDEA_Holding_Test3.Components;
+﻿using IDEA_Holding_Test3.Components;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components.Authorization;
+using IDEA_Holding_Test3.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Konekcija sa bazom podataka
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// Konfiguracija Identity korisničkog sistema
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+})
+    .AddRoles<IdentityRole>()  // Dodaj podršku za role (Admin, User)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// AuthenticationStateProvider - omogućava Blazor-u da koristi login sesije
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider>();
+
+// Podrška za Blazor Server i Razor komponente
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Konfiguracija middleware-a
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
@@ -17,10 +41,13 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.UseAuthentication();  // Omogućili autentifikaciju
+app.UseAuthorization();   // Omogućili autorizaciju
+
+// Omogućili Blazor da koristi Authentication & Authorization
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
