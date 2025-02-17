dbd995979b81b460f4a4b76b2bcdad4f3097f230
fejzo9 <fejzo999@gmail.com>
2025-02-17 14:35:18 +0100

    After running migrations, this files changed.

---
+++ IDEA_Holding_Test3/Migrations/20250217111558_IdentitySetup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IDEA_Holding_Test3.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentitySetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
